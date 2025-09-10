1a68a24dc58643c8d2a6e159c12d981ef3f09a7d
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-10 13:34:22 +0100

    Time: 5 ms (34.98%), Space: 61.5 MB (17.6%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -4,19 +4,17 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    const results = [];
-    candidates.sort((a,b) => a-b);
-    const solver = (choices, target, pos) => {
-        if (target == 0) return results.push(choices);
-        if (target < candidates[0]) return;
-
-        for (let i=pos;i<candidates.length;i++) {
-            const choice = candidates[i];
-            if (target - choice >= 0) solver([...choices,choice], target - choice, i);
+    candidates.sort((a,b) => a-b)
+    const prelims = [];
+    const solver = (currentChoices,remainingTarget, pos) => {
+        if (remainingTarget == 0) {
+            prelims.push(currentChoices)
+        } else if (remainingTarget < candidates[0]) return;
+        for (let i=pos; i <candidates.length;i++) {
+            const choice = candidates[i]
+            if (remainingTarget - choice >= 0) solver([...currentChoices,choice], remainingTarget-choice,i);
         }
     }
-
     solver([],target,0);
-
-    return results;
+    return prelims
 };
