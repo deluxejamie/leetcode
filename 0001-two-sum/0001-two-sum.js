7fb20901b6fd2a82386674fe66a05b586bb196ac
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-10 12:48:55 +0100

    Time: 7 ms (47.96%), Space: 57 MB (9.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const numMap = new Map();
+	const map = nums.reduce(
+		(prev, val, index) => prev.set(val, index),
+		new Map()
+	);
 
-    for (let i=0;i<nums.length;i++) {
-        const targetNumber = nums[i];
-        if (numMap.has(targetNumber)) numMap.get(targetNumber).push(i);
-        else (numMap.set(targetNumber,[i]));
-    }
+	for (let i = 0; i < nums.length; i++) {
+		const res = map.get(target - nums[i]);
+		if (res && res != i) return [i, res];
+	}
 
-    for (let i=0;i<nums.length;i++) {
-        const required = target - nums[i];
-        if (numMap.has(required) && numMap.get(required).some(q => q != i)) return [i,numMap.get(required).find(q => q != i)];
-    }
+    
 };
