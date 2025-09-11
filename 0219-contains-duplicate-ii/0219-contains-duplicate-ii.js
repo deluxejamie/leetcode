efa9290729eb02ac152692097b7cecaad7ae9e35
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-11 15:24:36 +0100

    Time: 33 ms (44.58%), Space: 82.8 MB (46.89%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,7 +4,6 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    if (nums.length == 1) return false;
     const found = new Map();
     for (let i=0; i <nums.length;i++) {
         if (found.has(nums[i]) && Math.abs(found.get(nums[i]) -  i) <=k) return true;
