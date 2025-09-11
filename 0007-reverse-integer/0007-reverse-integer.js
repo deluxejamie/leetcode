4ac98b6c2bc218be1b1cb9e2cb63d42776e9ba72
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-11 15:04:25 +0100

    Time: 43 ms (86.7%), Space: 55.6 MB (44.65%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -7,7 +7,7 @@ const MIN = - (2 ** 31)
 const MAX = (2 ** 31) - 1;
 var reverse = function(x) {
     const isNegative = x<0 ? -1 : 1;
-    const result = parseInt(Math.abs(x).toString().split("").reverse().join(""))
+    const result = parseInt([...Math.abs(x).toString()].reverse().join(""))
     return (result < MIN || result > MAX) ? 0 : 
     result * isNegative;
 
