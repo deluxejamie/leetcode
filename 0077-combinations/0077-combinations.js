28b0557e36f3e571b627c0d2a2a738e0bb47ea2e
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-12 16:34:43 +0100

    Time: 71 ms (33.48%), Space: 129.6 MB (12.26%) - LeetHub

--- 0077-combinations/0077-combinations.js
+++ 0077-combinations/0077-combinations.js
@@ -7,11 +7,13 @@ var combine = function(n, k) {
     const results = [];
 
     const solver = (choices,position) => {
-        if (choices.length == k) results.push(choices);
+        if (choices.length == k) results.push([...choices]);
         
         for (let i=position; k - (choices.length) <= (n + 1 - i) && i <= n;i++) {
-            solver([...choices,i],i+1)
-        }
+            choices.push(i);
+            solver(choices,i+1);
+            choices.pop(i);
+            }
     }
 
     solver([],1);
