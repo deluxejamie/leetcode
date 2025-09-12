e1d8db412f5629035dd182ffabdde1789fe9f10c
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-12 15:53:06 +0100

    Time: 4 ms (33.7%), Space: 59.2 MB (5.43%) - LeetHub

---
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} customers
+ * @param {number[]} grumpy
+ * @param {number} minutes
+ * @return {number}
+ */
+var maxSatisfied = function(customers, grumpy, minutes) {
+    let bestGained = 0;
+    let bestEnd = minutes-1;
+    let normalTotal = 0;
+    for (let i=0;i<minutes;i++) {
+        bestGained+=customers[i] * (grumpy[i]);
+        normalTotal+=customers[i] * (1-grumpy[i]);
+    }
+    let workingGained = bestGained;
+
+    for (let end=minutes;end<customers.length;end++) {
+        workingGained+= (customers[end] * (grumpy[end])) - (customers[end-minutes] * (grumpy[end-minutes]));
+        normalTotal += customers[end] * (1-grumpy[end]);
+        if (workingGained > bestGained) {
+            bestGained = workingGained;
+            bestEnd = end;
+        }
+    }
+
+    for (let i=bestEnd+1-minutes;i<=(bestEnd);i++) {
+        normalTotal += (customers[i]) * (grumpy[i]);
+    }
+    return normalTotal
+
+    
+};
