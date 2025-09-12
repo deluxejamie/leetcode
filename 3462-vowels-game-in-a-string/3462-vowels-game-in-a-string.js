287464a8b2aaf3cc103ae5fee4b03cb9f6ce50cc
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-12 16:13:48 +0100

    Time: 7 ms (85.71%), Space: 59.9 MB (47.62%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.js
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var doesAliceWin = function(s) {
-    const vowels = new Set();
+    const vowels = new Map();
     for (const v of ["a","e","i","o","u"]) {
-        vowels.add(v.charCodeAt(0));
+        vowels.set(v.charCodeAt(0),true);
     }
     for (let i=0; i< s.length;i++) {
         if (vowels.has(s.charCodeAt(i))) return true
