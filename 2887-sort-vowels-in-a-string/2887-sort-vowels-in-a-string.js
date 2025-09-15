d83470bbf313d024c1a4fa4d95d749a65c879a35
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-15 22:05:28 +0100

    Time: 45 ms (88.42%), Space: 66.6 MB (87.93%) - LeetHub

---
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortVowels = function(s) {
+    const vowelPositions = [];
+    const vowelsContained = new Map();
+    for (let i=0;i<s.length;i++) {
+        if ("aeiou".includes(s[i].toLowerCase())) {
+            vowelPositions.push(i);
+        }
+        vowelsContained.set(s[i], (vowelsContained.get(s[i]) ?? 0) + 1);
+    };
+
+
+    let vowels = [..."AEIOU",..."aeiou"];
+    let currentVowelsPosition = 0;
+    const newString = [...s];
+    
+    for (let i=0;i<vowelPositions.length;i++) {
+        while ((vowelsContained.get(vowels[currentVowelsPosition]) ?? 0) == 0) currentVowelsPosition++;
+        newString[vowelPositions[i]] = vowels[currentVowelsPosition];
+        vowelsContained.set(vowels[currentVowelsPosition],vowelsContained.get(vowels[currentVowelsPosition])-1)
+    }
+    return newString.join("");
+    
+};
