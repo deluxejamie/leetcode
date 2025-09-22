2eb4058a950e31b15490879f31cf4a99750aeb2b
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-22 12:59:35 +0100

    Time: 2 ms (72.32%), Space: 65.4 MB (13.36%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let result = nums[0];
+    let bestTotal = nums[0];
+    for (let i=1;i<nums.length; i++) {
+        if (result < 0) result=0;
+        const newTotal = result+nums[i];
+        bestTotal = Math.max(bestTotal,newTotal);
+        result = newTotal;
+    }
+    return bestTotal;
+};
