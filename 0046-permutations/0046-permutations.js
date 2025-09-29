4caecffbaeb4113a3d112fff6d9db948ac09f6bf
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-29 14:29:54 +0100

    Time: 0 ms (100%), Space: 59.3 MB (73.51%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -6,18 +6,19 @@ var permute = function(nums) {
 
     const results = [];
 
-    const compute = (choices,remaining) => {
-        if (remaining.length == 0) {
-            results.push(choices)
-            return;
-        }
-        
-        for (const r of remaining) {
-            compute([...choices,r],remaining.filter(q => q!==r))
-        }
-    }
-    compute([],nums);
+    const compute = (current) => {
+        if (current == nums.length) return results.push([...nums]);
 
+        for (let pointer = current; pointer < nums.length;pointer++) {
+            const temp = nums[current];
+            nums[current] = nums[pointer];
+            nums[pointer] = temp;
+            compute(current+1);
+            nums[pointer] = nums[current];
+            nums[current] = temp;
+        }     
+    }
+    compute(0);
     return results;
     
 };
