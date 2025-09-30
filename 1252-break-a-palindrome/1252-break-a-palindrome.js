4eb9c17c76d8bd0dacc6d84b830557c3f453dfe8
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-30 14:23:01 +0100

    Time: 0 ms (100%), Space: 54.4 MB (25%) - LeetHub

---
+++ 1252-break-a-palindrome/1252-break-a-palindrome.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} palindrome
+ * @return {string}
+ */
+var breakPalindrome = function(palindrome) {
+    if (palindrome.length== 1) return "";
+    let updatedString = [...palindrome]
+
+    for (let i=0;i<Math.ceil((palindrome.length-1)/2);i++) {
+        if (palindrome[i] != "a") {
+            updatedString[i] = "a"
+            return updatedString.join("");
+        }
+    }
+    for (let i=palindrome.length-1;i>=0;i--) {
+        if (palindrome[i] != "b") {
+            updatedString[i] = "b"
+            return updatedString.join("")
+        }
+    }
+    
+    
+    
+};
