ff237aadd0ac3f55a039318bc783e5c9343a74bb
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-30 12:56:22 +0100

    Time: 6 ms (100%), Space: 60.4 MB (27.08%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -5,11 +5,16 @@
 var winnerOfGame = function(colors) {
     let alice = 0;
     let bob = 0;
-    for (let i = 1; i<colors.length-1; i++) {
-        if (colors[i] == colors[i-1] && colors[i] == colors[i+1]) {
-            if (colors[i] == "A") alice++;
+    let prev2 = colors[0];
+    let prev1 = colors[1]
+    for (let i = 2; i<colors.length; i++) {
+        
+        if (prev1 == prev2 && prev2 == colors[i]) {
+            if (prev1 == "A") alice++;
             else bob++;
         }
+        prev2 = prev1;
+        prev1 = colors[i];
     }
     return alice > bob;
     
