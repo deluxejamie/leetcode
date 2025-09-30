bc3f047c5993949d93a9deab8e5665948603eaea
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-09-30 20:17:17 +0100

    Time: 1 ms (90.74%), Space: 55.8 MB (53.7%) - LeetHub

--- 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.js
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.js
@@ -4,23 +4,19 @@
  * @return {boolean}
  */
 var checkAlmostEquivalent = function(word1, word2) {
-    const chars = new Set();
-    const freqs1 = new Map();
-    const freqs2 = new Map();
+    const freqs = new Map();
 
     for (let i=0;i<word1.length;i++) {
         const char = word1[i];
-        freqs1.set(char,(freqs1.get(char) ?? 0) +1);
-        chars.add(char);
+        freqs.set(char,(freqs.get(char) ?? 0) +1);
     }
     for (let i=0;i<word2.length;i++) {
         const char = word2[i];
-        freqs2.set(char,(freqs2.get(char) ?? 0) +1);
-        chars.add(char);
+        freqs.set(char,(freqs.get(char) ?? 0) -1);
     }
-    
-    for (const char of chars) {
-        if (Math.abs((freqs2.get(char) ?? 0) - (freqs1.get(char) ?? 0)) > 3) return false;
+
+    for (const [char,val] of freqs.entries()) {
+        if (Math.abs(val) > 3) return false;
     }
     return true;
 
