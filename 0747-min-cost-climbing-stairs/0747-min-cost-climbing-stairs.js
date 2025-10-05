ff16d5167d2b0ff71a4fc82a4600daf39a771d62
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-10-05 19:23:53 +0100

    Time: 0 ms (100%), Space: 54.7 MB (63.35%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
@@ -3,20 +3,7 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-
-    const dp = new Array(cost.length);
-    
-    const solve = (index) => {
-        if (index == cost.length) return 0;
-        if (index >= cost.length - 2) return cost[index];
-
-        if (dp[index] != undefined) return dp[index];
-
-        const res = cost[index] + Math.min(solve(index+1), solve(index+2));
-        dp[index] = res;
-        return res;
-
-    } 
-    return Math.min(solve(0), solve(1));
+    for (let i=cost.length-3;i>=0;i--) cost[i]+= Math.min(cost[i+1],cost[i+2])
+    return Math.min(cost[0], cost[1]);
     
 };
