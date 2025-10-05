bbce479138cd0b2c5b5dd5043668352fd80e3e38
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-10-05 13:07:27 +0100

    Time: 0 ms (100%), Space: 57 MB (18.57%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.js
+++ 0263-ugly-number/0263-ugly-number.js
@@ -4,10 +4,10 @@
  */
 var isUgly = function(n) {
     if (n == 0) return false;
-    let num = n;
-    while (num % 2 == 0) num/=2;
-    while (num % 3 == 0) num/=3;
-    while (num % 5 == 0) num/=5;
-    return num == 1;
+
+    while (n % 2 == 0) n/=2;
+    while (n % 3 == 0) n/=3;
+    while (n % 5 == 0) n/=5;
+    return n == 1;
     
 };
