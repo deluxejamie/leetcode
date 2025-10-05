938be0f7559468966331eaee7327d3233730d4ee
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-10-05 18:20:24 +0100

    Time: 2 ms (25.15%), Space: 55.7 MB (27.5%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.js
+++ 0091-decode-ways/0091-decode-ways.js
@@ -13,7 +13,7 @@ var numDecodings = function(s) {
     let thisResult = numDecodings(s.slice(1));  // single-digit
 
     if (s.length >= 2) {
-        const twoDigit = parseInt(s.slice(0,2), 10);
+        const twoDigit = parseInt(s.slice(0,2));
         if (twoDigit >= 10 && twoDigit <= 26) {
             thisResult += numDecodings(s.slice(2));
         }
