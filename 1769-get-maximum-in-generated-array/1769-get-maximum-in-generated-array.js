0a6970b47873d508c35a104d4a3c520ffd992847
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-10-06 17:13:10 +0100

    Time: 10 ms (4.65%), Space: 57.9 MB (6.98%) - LeetHub

--- 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.js
+++ 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.js
@@ -4,7 +4,9 @@
  */
 var getMaximumGenerated = function(n) {
     if (n == 0) return 0;
-    const nums = [0,1];
+    const nums = new Array(n+1);
+    nums[0] = 0;
+    nums[1] = 1;
     let max = 1;
 
     let currentOdd = true;
