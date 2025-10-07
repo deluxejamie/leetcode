9b83115801b951f80b9babdf270b868c5434c8f9
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-10-07 17:15:33 +0100

    Time: 40 ms (52.91%), Space: 80.4 MB (57.24%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,47 +3,21 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if (nums.length == 0) return 0;
-    const dupes = new Set();
-    const needAbove = new Map();
-    const needBelow = new Map();
-    let maxLength = 0;
-
+    const set = new Set();
     for (let i=0;i<nums.length;i++) {
-        if (dupes.has(nums[i])) continue;
-        dupes.add(nums[i]);
-        
-        if (needBelow.has(nums[i])) {
-            const data = needBelow.get(nums[i]);
-            data.below--;
-            needBelow.delete(nums[i]);
-
-            if (needAbove.has(nums[i])) {
-                const joinedBelow = needAbove.get(nums[i]);
-                data.below = joinedBelow.below;
-                needAbove.delete(nums[i]);
-                needBelow.delete(joinedBelow.below);
-                needBelow.set(data.below,data);
-            } else {
-                needBelow.set(nums[i]-1,data);
-            }
-            maxLength = Math.max(maxLength, (data.above - (data.below+1)))
-
-        } else if (needAbove.has(nums[i])) {
-            const data = needAbove.get(nums[i])
-            data.above++;
-            needAbove.delete(nums[i]);
-            needAbove.set(nums[i]+1,data);
-            maxLength = Math.max(maxLength,(data.above - (data.below+1)));
-        } else {
-            const seq = {
-                above:nums[i]+1,
-                below:nums[i]-1
-            };
-            maxLength = Math.max(maxLength,1);
-            needAbove.set(nums[i]+1, seq);
-            needBelow.set(nums[i]-1, seq);
+        set.add(nums[i]);
+    }
+    let maxLength = 0;
+    for (const num of set) {
+        // not the start of the sequence
+        if (set.has(num-1)) continue;
+        let sequenceLength = 1;
+        let j=num+1;
+        while(set.has(j)) {
+            sequenceLength++;
+            j++;
         }
+        maxLength = Math.max(maxLength,sequenceLength)
     }
     return maxLength;
 
