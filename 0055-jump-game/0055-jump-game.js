3cfe8c4e367a6dd0ae07ea80e80fd0b887266ab0
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-10-23 21:17:59 +0100

    Time: 1 ms (83.62%), Space: 60.1 MB (26.11%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -22,17 +22,24 @@ var canJump = function(nums) {
         // return visitPos(0)
 
     // bottom up approach
-    const reachesSolution = new Set();
-    reachesSolution.add(nums.length-1)
-    for (let i=nums.length-2;i>=0;i--) {
-        for (let j=1;j<=nums[i];j++) {
-            if (reachesSolution.has(i+j)) {
-                reachesSolution.add(i);
-                break;
-            }
-        }
+    // const reachesSolution = new Set();
+    // reachesSolution.add(nums.length-1)
+    // for (let i=nums.length-2;i>=0;i--) {
+    //     for (let j=1;j<=nums[i];j++) {
+    //         if (reachesSolution.has(i+j)) {
+    //             reachesSolution.add(i);
+    //             break;
+    //         }
+    //     }
+    // }
+    // return reachesSolution.has(0);
+
+    let maxSteps = nums[0];
+    for (let i=0;i<nums.length;i++) {
+        if (i == nums.length-1) return true;
+        maxSteps = Math.max(maxSteps-1,nums[i]);
+        if (maxSteps == 0) return false;
     }
-    return reachesSolution.has(0);
     
 
     
