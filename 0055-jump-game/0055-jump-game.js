6bd995196ebcbb018d2ae5606e415a3d23d0adcf
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-10-23 14:17:07 +0100

    Time: 1617 ms (5.01%), Space: 66 MB (5.05%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -8,13 +8,14 @@ var canJump = function(nums) {
     const visitPos = (i) => {
         if (visited.has(i)) return false;
         if (i == nums.length-1) return true;
+        visited.add(i);
 
         for (let p=1;p<=nums[i];p++) {
             if (visitPos(p+i)) {
                 return true;
+            } else {
             }
         }
-        visited.add(i);
         return false;
     }
     return visitPos(0)
