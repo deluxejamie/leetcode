8c25fb10ec7dc8327c16315ceb6ebd0aad2e8e73
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-10-26 20:36:46 +0000

    Time: 477 ms (5.02%), Space: 66.1 MB (7.02%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+
+const ops = {
+    "+": (a,b) => a+b,
+    "-": (a,b) => a-b,
+    "*": (a,b) => a*b,
+    "/": (a,b) => Math.trunc(a/b)
+    }
+
+var evalRPN = function(tokens) {
+    const stack = [];
+    for (let i=0;i<tokens.length;i++) {
+        if (tokens[i] in ops) {
+            const b = stack.pop();
+            const a = stack.pop();
+            stack.push((ops[tokens[i]])(a,b));
+        } else {
+            stack.push(parseInt(tokens[i]));
+        }
+        console.log(stack)
+    }
+    return stack[0];
+};
