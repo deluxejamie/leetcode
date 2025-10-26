344ef021be0a8e64aa1225085f81357ea19f4533
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-10-26 19:52:59 +0000

    Time: 6 ms (46.67%), Space: 58.6 MB (25.71%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.js
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.js
@@ -3,9 +3,39 @@
  * @return {string}
  */
 var removeDuplicateLetters = function(s) {
-    const occurences = new Map(); // remaining occurences including any in stack
+
+    // approach 1 (intuition based)
+    // const occurences = new Map(); // remaining occurences including any in stack
+    // for (let i=0;i<s.length;i++) {
+    //     occurences.set(s[i], (occurences.get(s[i]) ?? 0) + 1); 
+    // }
+    // const stack = [];
+    // const used = new Set();
+    // for (let i=0;i<s.length;i++) {
+    //     if (!used.has(s[i])) {
+    //         while (
+    //             stack.length > 0 &&
+    //             occurences.get(stack[stack.length-1]) > 1 &&
+    //             stack[stack.length-1].localeCompare(s[i]) >= 0
+    //         ) {
+
+    //             const top = stack.pop();
+    //             occurences.set(top, occurences.get(top)-1);
+    //             used.delete(top);
+    //         }
+    //         stack.push(s[i]);
+    //         used.add(s[i])
+    //     } else {
+    //         occurences.set(s[i],occurences.get(s[i])-1);
+    //     }
+    // }
+    // return stack.join('')
+    
+
+    // approach 2: reducing the number of unnecessary operations
+    const occurences = new Map(); // Map {char => index of final occurence}
     for (let i=0;i<s.length;i++) {
-        occurences.set(s[i], (occurences.get(s[i]) ?? 0) + 1); 
+        occurences.set(s[i], i); 
     }
     const stack = [];
     const used = new Set();
@@ -13,21 +43,16 @@ var removeDuplicateLetters = function(s) {
         if (!used.has(s[i])) {
             while (
                 stack.length > 0 &&
-                occurences.get(stack[stack.length-1]) > 1 &&
+                occurences.get(stack[stack.length-1]) > i &&
                 stack[stack.length-1].localeCompare(s[i]) >= 0
             ) {
 
                 const top = stack.pop();
-                occurences.set(top, occurences.get(top)-1);
                 used.delete(top);
             }
             stack.push(s[i]);
             used.add(s[i])
-        } else {
-            occurences.set(s[i],occurences.get(s[i])-1);
         }
     }
     return stack.join('')
-
-    
 };
