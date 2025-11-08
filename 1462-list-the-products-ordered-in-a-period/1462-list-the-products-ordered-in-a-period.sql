b22eefc0bf7fac609a65549d872075507c5d07ab
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-11-08 21:11:40 +0000

    Time: 682 ms (54.23%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 SELECT product_name, SUM(unit) unit
-FROM products NATURAL JOIN (SELECT * FROM orders WHERE order_date < '2020-03-01' AND order_date >= '2020-02-01') orders
+FROM products NATURAL JOIN orders
+WHERE order_date < '2020-03-01' AND order_date >= '2020-02-01'
 GROUP BY product_name
 HAVING unit >= 100;
