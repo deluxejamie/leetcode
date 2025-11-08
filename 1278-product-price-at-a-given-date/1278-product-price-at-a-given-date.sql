10876cd8b704629a7749aa3f9de99806b8968adc
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-11-08 14:21:13 +0000

    Time: 637 ms (25.72%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -3,7 +3,7 @@
 SELECT product_id, COALESCE(new_price,10) price FROM
 (SELECT DISTINCT product_id FROM products) t1 NATURAL LEFT OUTER JOIN (
  (
-     SELECT DISTINCT product_id, MAX(change_date) AS change_date
+     SELECT product_id, MAX(change_date) AS change_date
      FROM (SELECT * FROM products WHERE change_date <= '2019-08-16') s
      GROUP BY product_id
  ) t2 NATURAL JOIN products
