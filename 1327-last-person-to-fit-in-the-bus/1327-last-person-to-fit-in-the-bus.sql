a089c0eded525db342a2fbf607b65b869fdb0f9b
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-11-09 19:28:45 +0000

    Time: 1710 ms (12.52%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,12 +1,7 @@
 # Write your MySQL query statement below
-SELECT person_name
-FROM queue
-WHERE turn = (
-    SELECT MAX(turn) FROM (
-        SELECT turn FROM 
-        (SELECT * FROM queue ORDER BY turn ASC) q
-        WHERE (
-            SELECT SUM(weight) weight FROM queue t2 WHERE t2.turn <= q.turn 
-        ) <= 1000
-    ) t3
-);
+SELECT m.person_name
+FROM queue m INNER JOIN queue s ON s.turn <= m.turn
+GROUP BY m.turn
+HAVING SUM(s.weight) <= 1000
+ORDER BY SUM(s.weight) DESC
+LIMIT 1;
