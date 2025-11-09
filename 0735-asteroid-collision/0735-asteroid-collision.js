f1963d4430c23df1563631993d2c58b5963e53c4
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-11-09 20:51:51 +0000

    Time: 7 ms (26.69%), Space: 58 MB (25.03%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.js
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -9,13 +9,11 @@ var asteroidCollision = function(asteroids) {
         remaining.push(asteroid);
         // moving towards eachother
         while (remaining.length > 1 && remaining[remaining.length-1] < 0 && remaining[remaining.length-2] > 0) {
-            const a = remaining.pop();
-            const absA = Math.abs(a);
-            const b = remaining.pop();
-            const absB = Math.abs(b);
-            if (absA == absB) continue;
-            else if (absA > absB) remaining.push(a);
-            else remaining.push(b)
+            const right = remaining.pop();
+            const left = remaining.pop();
+            if (left == -right) continue;
+            else if (left > -right) remaining.push(left);
+            else remaining.push(right)
         }
     }
     return remaining;
