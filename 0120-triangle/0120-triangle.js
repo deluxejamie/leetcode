abb3bc335f00bdee8d7e269887e99e1d66e9eb5f
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-11-15 13:02:39 +0000

    Time: 12 ms (6.96%), Space: 56.6 MB (36.02%) - LeetHub

--- 0120-triangle/0120-triangle.js
+++ 0120-triangle/0120-triangle.js
@@ -3,20 +3,24 @@
  * @return {number}
  */
 var minimumTotal = function(triangle) {
-    for (let i=1;i<triangle.length;i++) {
-        for (let j=0;j<triangle[i].length;j++) {
-            const left = j == 0 ? Number.POSITIVE_INFINITY : triangle[i-1][j-1];
-            const right = j == triangle[i].length-1 ? Number.POSITIVE_INFINITY : triangle[i-1][j];
-            triangle[i][j]+= Math.min(right, left);
+    // top down
+    // for (let i=1;i<triangle.length;i++) {
+    //     for (let j=0;j<triangle[i].length;j++) {
+    //         const left = j == 0 ? Number.POSITIVE_INFINITY : triangle[i-1][j-1];
+    //         const right = j == triangle[i].length-1 ? Number.POSITIVE_INFINITY : triangle[i-1][j];
+    //         triangle[i][j]+= Math.min(right, left);
+    //     }
+    // }
+    // return Math.min(...triangle[triangle.length-1]);
+
+    // bottom up
+    for (let i=triangle.length-2;i>=0;i--) {
+        for (let j=0;j<=triangle.length;j++) {
+            triangle[i][j] += Math.min(triangle[i+1][j],triangle[i+1][j+1]);
         }
     }
-    let min = Number.POSITIVE_INFINITY;
-    const finalRow = triangle[triangle.length-1];
-    for (let i=0;i<finalRow.length;i++) {
-        min = Math.min(min, finalRow[i])
-    }
 
-    return min;
+    return triangle[0][0];
 
     
 };
