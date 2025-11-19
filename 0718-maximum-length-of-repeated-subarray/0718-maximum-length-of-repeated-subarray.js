0d7e28db60ac3cdc3bc81a4c75e2f8207994a990
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-11-19 13:40:21 -0800

    Time: 117 ms (85.12%), Space: 87.5 MB (19.01%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.js
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.js
@@ -5,16 +5,31 @@
  */
 var findLength = function(nums1, nums2) {
 
-    let max = Number.NEGATIVE_INFINITY;
-    // dp[i][j] is the longest subsequent ending at text1[i] and text2[j]
+    let max = 0;
+    // dp[i][j] is the longest subsequent ending at nums1[i] and nums2[j]
     const dp = new Array(nums1.length);
     for (let i=0;i<nums1.length;i++) {
         dp[i] = new Array(nums2.length);
     }
+
+    // setup first row and columns of dp
+    for (let j=0; j<nums2.length;j++) {
+        if (nums1[0] == nums2[j]) {
+            dp[0][j] = 1;
+            max = 1;
+        } else { dp[0][j] = 0; }
+    }
+
     for (let i=0;i<nums1.length;i++) {
-        for (let j=0;j<nums2.length;j++) {
-            const fromBefore = j ==0 || i ==0 ? 0 : dp[i-1][j-1];
-            const outcome = nums1[i] == nums2[j] ? 1 + fromBefore : 0;
+        if (nums1[i] == nums2[0]) {
+            dp[i][0] = 1;
+            max = 1;
+        } else { dp[i][0] = 0; }
+    }
+
+    for (let i=1;i<nums1.length;i++) {
+        for (let j=1;j<nums2.length;j++) {
+            const outcome = nums1[i] == nums2[j] ? dp[i-1][j-1] + 1 : 0;
             dp[i][j] = outcome;
             max = Math.max(max,outcome);
         }
