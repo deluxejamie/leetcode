3d53a49fed7174a29be9512f8ff716f8caa6dacb
Jamie <86162936+deluxejamie@users.noreply.github.com>
2025-12-20 11:56:06 +0000

    Time: 7 ms (72.41%), Space: 58.2 MB (86.21%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
@@ -6,7 +6,7 @@ var minDeletionSize = function(strs) {
     let deleted = 0;
     for (let i=0;i<strs?.[0].length || 0;i++) {
         for (let j=1;j<strs.length;j++) {
-            if (strs[j][i].localeCompare(strs[j-1][i]) < 0) {
+            if (strs[j].charCodeAt(i) < strs[j-1].charCodeAt(i)) {
                 deleted++;
                 break;
             }
