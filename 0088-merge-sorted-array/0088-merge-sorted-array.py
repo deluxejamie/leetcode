68cd00eb0a5289bf1cc7123e45b0fc1b55d7d16c
Jamie <86162936+deluxejamie@users.noreply.github.com>
2026-02-03 11:28:51 -0800

    Time: 3 ms (7.71%), Space: 19.4 MB (20.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,6 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+
+        # idea 1 (using space)
+        """
         i, j = 0, 0
         res = []
         for _ in range(m+n):
@@ -18,10 +21,30 @@ class Solution:
             else:
                 res.append(nums2[j])
                 j+=1
-            
         for i in range(0,m+n):
             nums1[i] = res[i]
-
         
         return res
+        """
 
+        # idea 2 (work in reverse order)
+
+        i, j = m-1,n-1
+        for x in range(len(nums1)-1,-1,-1):
+            if j>=0 and i>=0:
+                if nums1[i] > nums2[j]:
+                    nums1[x] = nums1[i]
+                    i-=1
+                else:
+                    nums1[x] = nums2[j]
+                    j-=1
+            else:
+                if j >=0:
+                    nums1[x] = nums2[j]
+                    j-=1
+                else:
+                    break
+            
+
+
+                
