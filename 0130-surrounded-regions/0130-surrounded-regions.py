784d09dc83e2f290fe59affee6fafcecaa1221b5
Jamie <86162936+deluxejamie@users.noreply.github.com>
2026-02-03 04:21:46 -0800

    Time: 11 ms (26.17%), Space: 26.6 MB (14.96%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -6,34 +6,28 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
 
-        nonouters = set()
-        outers = set()
+        unvisited = set()
+        frontier = deque()
         for i in range(0,len(board)):
             for j in range(0,len(board[0])):
                 if board[i][j] == "O":
                     if i == 0 or i == len(board)-1 or j == 0 or j == len(board[0])-1:
-                        outers.add((i,j))
-                    else:
-                        nonouters.add((i,j))
+                        frontier.append((i,j))
+                    
+                    unvisited.add((i,j))
 
-        while len(outers) != 0:
-            frontier = deque([next(iter(outers))])
-        
-            while len(frontier) != 0:
-                (x,y) = frontier.popleft()
-                if (x,y) in nonouters:
-                    nonouters.remove((x,y))
-                elif (x,y) in outers:
-                    outers.remove((x,y))
-                else:
-                    continue
-                
-                if (x-1,y) in nonouters: frontier.append((x-1,y))
-                if (x+1,y) in nonouters: frontier.append((x+1,y))
-                if (x,y-1) in nonouters: frontier.append((x,y-1))
-                if (x,y+1) in nonouters: frontier.append((x,y+1))
+        while len(frontier) != 0:
+            (x,y) = frontier.popleft()
+            if (x,y) not in unvisited:
+                continue
+            unvisited.remove((x,y))
+           
+            if (x-1,y) in unvisited: frontier.append((x-1,y))
+            if (x+1,y) in unvisited: frontier.append((x+1,y))
+            if (x,y-1) in unvisited: frontier.append((x,y-1))
+            if (x,y+1) in unvisited: frontier.append((x,y+1))
 
-        for (x,y) in nonouters:
+        for (x,y) in unvisited:
             board[x][y] = "X"
 
             
